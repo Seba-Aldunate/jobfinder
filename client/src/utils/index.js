c7895502dae9ffd08b7aaccc5458b84481ccc96e
Seba-Aldunate <csaldunate@gmail.com>
2023-09-21 12:48:34 +0200

    deplay and change API_URL

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,5 +1,7 @@
 import axios from "axios";
-const API_URL = "http://localhost:8801/api-v1";
+// const API_URL = "http://localhost:8801/api-v1";
+const API_URL = "https://jobfinder-d848.onrender.com/api-v1";
+
 
 export const API = axios.create({
   baseURL: API_URL,
